9d478ad706fa65f5d0d28d7cf9e4b9370795f49a
glpolj <glpolj@ttu.ee>
2022-06-06 13:17:56 +0300

    1

--- retrieve_repos.py
+++ retrieve_repos.py
@@ -3,6 +3,8 @@ import requests, json
 username = "talisainen"
 def retrieve_repos(username):
     
-  url = requests.get(f"https://api.github.com/users/{username}")
+  url = requests.get(f"https://api.github.com/users/{username}/repos")
 
   return url
+
+print(retrieve_repos(username))
